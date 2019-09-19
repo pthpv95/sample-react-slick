f7708ba68609058c87f31a461910a4dc2b3c22d2
Hien Pham <hien.pham@unicorn-fintech.com>
2019-09-19 19:36:28 +0700

    Init sample react-slick app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+// import './scss/style.scss';
+import 'jquery/dist/jquery';
+import 'bootstrap/dist/js/bootstrap.bundle';
 
+// import logo from './logo.svg';
+import SimpleSlider from './SimpleSlider';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SimpleSlider />
     </div>
   );
 }
